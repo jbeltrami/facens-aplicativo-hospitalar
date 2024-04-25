4756f73f3b4399f19d0ea557510a7e166f7ec17d
Murilo Possari Ferris <166919237+Murilaom@users.noreply.github.com>
2024-04-25 17:48:02 +0000

    Parte 7 - update pronta

--- models/procedimento.py
+++ models/procedimento.py
@@ -25,18 +25,32 @@ class Procedimento():
 #Conectar com Utils para suprir arquivo json base
 			self.__utils.create_data(novo_procedimento, Config().config_procedimento)
 			print("\nProcedimento cadastrado com sucesso")
-	
 
 		
 	def read(self):
 		lista_procedimento = self.__utils.read_data(Config().config_procedimento)
 		print(lista_procedimento)
 
-
-
 	def update(self):
-		# Código para atualizar um procedimento no banco de dados
-		pass
+# Código para atualizar um procedimento no banco de dados
+# Ler lista de pacientes
+		procedimento = self.__utils.read_data(Config().config_procedimento)
+		print(procedimento)
+		linha_do_procedimento = int(input('Escolha o numero do procedimento que deseja alterar na lista acima: '))
+		
+		
+		# Verifica se o procedimento existe
+		if linha_do_procedimento not in procedimento.index:
+			print("Procedimento não Cadastrado")
+			return False
+		
+		print(procedimento.loc[linha_do_procedimento])
+
+		input_atualizacao = input('Atualize o nome do Procedimento Escolhido: ')
+		procedimento.at[linha_do_procedimento,"nome"] = input_atualizacao
+
+		#sobreescrever informação da lista de procedimentos
+		self.__utils.update_data(procedimento, Config().config_procedimento)
 
 	def delete(self):
 		# Código para deletar um procedimento no banco de dados
