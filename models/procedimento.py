b719871f9f304bf1404dda9dd70f21ee8b640b95
Murilo Possari Ferris <166919237+Murilaom@users.noreply.github.com>
2024-04-25 17:16:17 +0000

    Código 6 pronto - Leitura do arquivo de Procedimentos

--- models/procedimento.py
+++ models/procedimento.py
@@ -1,18 +1,39 @@
-from utils.utils import Utils
+from utils.utils_procedimentos import Utils_Procedimento
 from config.config import Config
+import pandas as pd
 
 class Procedimento():
 	def __init__(self, nome = None):
 		self.__nome = nome
-		self.__utils = Utils()
+		self.__utils = Utils_Procedimento()
 		self.__config = Config()
 
 	def create(self):
 		# Código para inserir um procedimento no banco de dados
-		pass
+		input_procedimento = input("Digitar procedimento realizado para cadastro: ")
 
+		# Ler lista de procedimentos existente
+		procedimento = self.__utils.read_data(Config().config_procedimento)
+
+
+		if input_procedimento in procedimento['nome'].values:
+			print("Procedimento já cadastrado")
+			return False
+		else:
+			novo_procedimento = pd.DataFrame( {
+				'nome': [input_procedimento]}
+			)
+
+		print(input_procedimento)
+		print(procedimento)
+
+
+		
 	def read(self):
-		return self.__utils.read_data('./data/dados_procedimentos.json')
+		lista_procedimento = self.__utils.read_data(Config().config_procedimento)
+		print(lista_procedimento)
+
+
 
 	def update(self):
 		# Código para atualizar um procedimento no banco de dados
@@ -21,9 +42,6 @@ class Procedimento():
 	def delete(self):
 		# Código para deletar um procedimento no banco de dados
 		pass
-
-
-
 '''
 	{
 		'nome': 'nome_do_procedimento',
